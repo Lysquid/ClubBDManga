a73b9e6e81088e827191c87c25a6137c6f6e1c94
Lysquid <romainbenoit21560@gmail.com>
2023-09-20 09:22:05 +0200

    added log entries view to admin panel

--- clubBDM/admin.py
+++ clubBDM/admin.py
@@ -1,4 +1,36 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 
 admin.site.site_title = "Club BDManga"
 admin.site.site_header = "Club BDManga"
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'action_flag',
+        'object_repr'
+    ]
+
+    list_filter = [
+        'action_flag',
+        'user',
+        'content_type'
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
