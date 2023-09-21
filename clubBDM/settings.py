1e8c5d1b16c3a25edbebea214e15bd35b1311892
Lysquid <romainbenoit21560@gmail.com>
2023-09-21 06:12:07 +0200

    fix CSRF bug

--- clubBDM/settings.py
+++ clubBDM/settings.py
@@ -32,6 +32,11 @@ if DEBUG:
         '127.0.0.1',
         '0.0.0.0',
     ]
+    CSRF_TRUSTED_ORIGINS = [
+        'http://localhost:8001',
+        'http://127.0.0.1:8001',
+        'http://0.0.0.0:8001',
+    ]
 else:
     ALLOWED_HOSTS = [os.getenv('SITE_URL')]
 
