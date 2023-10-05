969a12009a4f0c5d77a3a5ec0431023d2bfbcca1
Lysquid <romainbenoit21560@gmail.com>
2023-10-05 13:49:30 +0200

    fix password asked each time for local development

--- bdmanga/settings.py
+++ bdmanga/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.getenv('SECRET_KEY', 'DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG', 0)))
