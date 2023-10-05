092df4c336ab780bc7a00020de5e5bc2d382f4d0
insalgo <insalgo.contact@gmail.com>
2023-10-05 09:28:21 +0000

    trying to allow external POST without success

--- asso/urls.py
+++ asso/urls.py
@@ -4,5 +4,6 @@ from asso import views
 
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
+    path("test", views.test),
     path("helloasso/notifs", views.helloasso_notif)
 ]
--- asso/views.py
+++ asso/views.py
@@ -10,13 +10,20 @@ class HomePageView(generic.TemplateView):
     template_name = "asso/home.html"
 
 
+def test(request: HttpRequest):
+    print("test received")
+    print(request.method)
+    print(request.POST)
+    return HttpResponse()
+
 def helloasso_notif(request: HttpRequest):
     if request.method != "POST":
         return HttpResponseNotAllowed(["POST"])
 
-    data = request.POST.dict()
     Path("helloasso/notifs/").mkdir(parents=True, exist_ok=True)
-    with open(f"helloasso/notifs/{int(time() * 1000)}.json", 'w') as f:
-        dump(data, f, indent=2)
+    with open(f"helloasso/notifs/content_{int(time() * 1000)}.json", 'w') as f:
+        dump(request.POST.dict(), f, indent=2)
+    with open(f"helloasso/notifs/headers_{int(time() * 1000)}.json", 'w') as f:
+        dump(dict(request.headers), f, indent=2)
 
     return HttpResponse()
