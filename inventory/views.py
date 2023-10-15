dad6d2bcc2ee65c3fb72491c4c3b51a73bff5dc5
insalgo <insalgo.contact@gmail.com>
2023-10-15 14:14:42 +0000

    working search in series

--- inventory/views.py
+++ inventory/views.py
@@ -6,13 +6,17 @@ from inventory.models import Series
 class SeriesListView(generic.ListView):
     model = Series
 
-
-class SeriesDetailView(generic.DetailView):
-    model = Series
+    # queryset = Series.objects.filter(name__icontains="test")
     
-    def get_queryset(self):  # new
-        object_list = Series.objects
+    def get_queryset(self):
+        print("in get_queryset")
+        object_list = Series.objects.all()
         query = self.request.GET.get("search")
+        print(query)
         if query:
             object_list = object_list.filter(name__icontains=query)
         return object_list
+
+
+class SeriesDetailView(generic.DetailView):
+    model = Series
