de6c8cb4c39df69c10056b11f47735cd91b95606
insalgo <insalgo.contact@gmail.com>
2023-10-15 13:39:01 +0000

    test series search

--- inventory/views.py
+++ inventory/views.py
@@ -9,3 +9,10 @@ class SeriesListView(generic.ListView):
 
 class SeriesDetailView(generic.DetailView):
     model = Series
+    
+    def get_queryset(self):  # new
+        object_list = Series.objects
+        query = self.request.GET.get("search")
+        if query:
+            object_list = object_list.filter(name__icontains=query)
+        return object_list
