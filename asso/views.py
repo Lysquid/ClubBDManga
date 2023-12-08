a0707e69c035276f445a9373a5922aed669926e8
Lysquid <romainbenoit21560@gmail.com>
2023-12-08 03:53:25 +0100

    improvements

--- asso/views.py
+++ asso/views.py
@@ -1,5 +1,14 @@
 from django.views import generic
+from inventory.models import Book, Series
+from asso.models import Member
 
 
 class HomePageView(generic.TemplateView):
     template_name = "asso/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books_count"] = Book.objects.count()
+        context["series_count"] = Series.objects.count()
+        context["member_count"] = Member.objects.filter(has_paid__exact=True).count()
+        return context
--- inventory/urls.py
+++ inventory/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from inventory import views
 
 urlpatterns = [
-    path("series/", views.SeriesListView.as_view(), name="series list"),
-    path("series/<str:pk>/", views.SeriesDetailView.as_view(), name="series detail"),
+    path("livres/", views.SeriesListView.as_view(), name="series list"),
+    path("livres/<str:pk>/", views.SeriesDetailView.as_view(), name="series detail"),
 ]
