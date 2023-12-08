e6e5de8b1c412b4ae9a9e72028a3926faa821654
Lysquid <romainbenoit21560@gmail.com>
2023-12-08 03:28:34 +0100

    series detail

--- inventory/views.py
+++ inventory/views.py
@@ -9,15 +9,27 @@ class SeriesListView(generic.ListView):
 
     def get_queryset(self):
         object_list = Series.objects.all()
+
         search = self.request.GET.get("search")
         if search:
             object_list = object_list.filter(Q(name__icontains=search) | Q(authors__name__icontains=search))
+
+        author = self.request.GET.get("author")
+        if author:
+            object_list = object_list.filter(authors__name__exact=author)
+
+        editor = self.request.GET.get("editor")
+        if editor:
+            object_list = object_list.filter(editors__name__exact=editor)
+
         book_type = self.request.GET.get("type")
         if book_type:
             object_list = object_list.filter(type__exact=book_type)
+
         genre = self.request.GET.get("genre")
         if genre:
             object_list = object_list.filter(genre__name__exact=genre)
+
         return object_list
 
     def get_context_data(self, *, object_list=None, **kwargs):
