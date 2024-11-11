b18458e13e897d4bd79821068609ad71fd49a133
Lysquid <8axop2rj@anonaddy.com>
2024-11-11 21:22:14 +0900

    default values when adding a new book

--- inventory/models.py
+++ inventory/models.py
@@ -84,20 +84,40 @@ class Series(models.Model):
         ordering = ["name"]
 
 
+def _last_book_series():
+    if Book.objects.exists() and Book.last_book_id:
+        return Book.objects.get(id=Book.last_book_id).series
+    return None
+
+
+def _next_volume_nb():
+    if Book.objects.exists() and Book.last_book_id:
+        return Book.objects.get(id=Book.last_book_id).volume_nb + 1
+    return None
+
+
+def _last_book_condition():
+    if Book.objects.exists() and Book.last_book_id:
+        return Book.objects.get(id=Book.last_book_id).condition
+    return None
+
+
 class Book(models.Model):
     id = models.CharField("cote", primary_key=True, max_length=12, editable=False, validators=[
         validators.RegexValidator('^[0-9]{2}[A-Z0-9]{5}[0-9]{5}$')
     ])
     name = models.CharField("nom", max_length=255, blank=True)
-    series = models.ForeignKey(Series, on_delete=models.CASCADE, verbose_name="série")
-    volume_nb = models.PositiveIntegerField("volume")
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, verbose_name="série", default=_last_book_series)
+    volume_nb = models.PositiveIntegerField("volume", default=_next_volume_nb)
     duplicate_nb = models.PositiveIntegerField("numéro de duplicata", default=1)
-    condition = models.PositiveSmallIntegerField("état", validators=[
+    condition = models.PositiveSmallIntegerField("état", default=_last_book_condition, validators=[
         validators.MinValueValidator(1),
         validators.MaxValueValidator(10)
     ])
     date_added = models.DateField("date d'ajout", auto_now_add=True)
     comment = models.TextField("commentaire", blank=True)
+
+    last_book_id = None
 
     @property
     def available(self):
@@ -109,6 +129,7 @@ class Book(models.Model):
 
     def save(self, *args, **kwargs):
         self.id = self.series.reference + str(self.volume_nb).zfill(3) + str(self.duplicate_nb).zfill(2)
+        Book.last_book_id = self.id
         super().save(*args, **kwargs)
 
     class Meta:
